c5c52c7f5ed190ed46b073bb8d35230f9c3e7a78
ishaimou <i.shaimoun@finamaze.com>
2021-10-16 19:34:08 +0100

    wip(IoT): install vagrant vbguest additions

--- p1/scripts/ishaimouS.sh
+++ p1/scripts/ishaimouS.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+export INSTALL_K3S_CHANNEL="stable"
+export INSTALL_K3S_EXEC="--bind-address=#{server_ip} --node-external-ip=#{server_ip} --flannel-iface=eth1"
+
+sudo -i
 systemctl disable firewalld --now
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="stable" sh -s -
-echo ks3 server installed successfully on ishaimouS Server Node
+curl -sfL https://get.k3s.io | sh -s -
+echo "Waiting for k3s to start"
+sleep 5
+cp /var/lib/rancher/k3s/server/token /vagrant_shared
+cp /etc/rancher/k3s/k3s.yaml /vagrant_shared
+echo "ks3 server installed and configured successfully on ishaimouS Server Node"
--- p1/scripts/ishaimouSW.sh
+++ p1/scripts/ishaimouSW.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+export INSTALL_K3S_CHANNEL="stable"
+export INSTALL_K3S_EXEC="--flannel-iface=eth1"
+export K3S_TOKEN_FILE=/vagrant_shared/token
+export K3S_URL=https://#{server_ip}:6443
+
+sudo -i
 systemctl disable firewalld --now
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="stable" \
-K3S_URL="https://ishaimouS:6443" KS3_TOKEN=`cat ` sh -s -
-echo ks3 agent installed successfully on ishaimouSW Worker Node
+curl -sfL https://get.k3s.io | sh -s -
+echo "ks3 agent installed and configured successfully on ishaimouSW Worker Node"
