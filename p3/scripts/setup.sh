8714c36075134d59e9f3c5a365c833c5ec83b595
ishaimou <ilyass.sh@gmail.com>
2021-10-30 15:43:53 +0100

    wip(IoT): part 3 done

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+# --- Environnement Setup --- #
+
 # Disable firewall
-# sudo systemctl disable firewalld --now
+sudo systemctl disable firewalld --now
 
 # Install Docker Engine on Centos8
-
 # Set up Docker repository
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -13,9 +14,14 @@ sudo yum install docker-ce docker-ce-cli containerd.io --allowerasing -y
 # Start Docker as a service
 sudo systemctl start docker
 
+# Post-installation Configuration
 # Manage Docker as a non-root user
 sudo usermod -aG docker $USER
 newgrp docker
+
+# Configure Docker to start on boot
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
 
 # Test Docker Installation
 docker run hello-world
@@ -24,11 +30,10 @@ docker run hello-world
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-echo "source <(kubectl completion bash)" >> ~/.bashrc
-echo "alias k='kubectl'" >> ~/.bashrc
-echo "complete -F __start_kubectl k" >> ~/.bashrc
+echo "source <(kubectl completion bash)" >>~/.bashrc
+echo "alias k='kubectl'" >>~/.bashrc
+echo "complete -F __start_kubectl k" >>~/.bashrc
 source ~/.bashrc
-
 
 # Install k3d Current Latest Release
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
@@ -47,28 +52,24 @@ kubectl create namespace argocd
 kubectl create namespace dev
 
 # Install ArgoCD
-kubectl apply -f https://github.com/argoproj/argo-cd/raw/stable/manifests/install.yaml -n argocd
+# https://github.com/argoproj/argo-cd/raw/stable/manifests/install.yaml
+# to disable TLS edit the argocd-server deployment to add the --insecure flag to the argocd-server command
+kubectl apply -f ../confs/install.yaml -n argocd
 # Wait for argocd-server to be ready
 kubectl rollout status deploy argocd-server -n argocd
-# to disable TLS edit the argocd-server deployment to add the --insecure flag to the argocd-server command
-# kubectl apply -f ../confs/install.yaml -n argocd
-
 # kubectl port-forward svc/argocd-server -n argocd 8080:443
-# Configure Ingress to access ArgoCD UI
+# Create Ingress to access ArgoCD UI
 kubectl apply -f ../confs/ingress.yaml -n argocd
 
-# Set ArgoCD Admin Password
-# kubectl -n argocd patch secret argocd-secret \
-#   -p '{"stringData": {
-#     "admin.password": "$2a$12$TOdvA6Ud4D.FY8tP3OWDdOWGAeJ4xZ6YUPSjAjKydhrqhZieUEIaS",
-#     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
-#   }}'
-
 # Get default password for the admin user
-kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
+# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+# Set ArgoCD Admin Password to <admin>
+kubectl -n argocd patch secret argocd-secret \
+    -p '{"stringData": {
+    "admin.password": "$2a$12$TOdvA6Ud4D.FY8tP3OWDdOWGAeJ4xZ6YUPSjAjKydhrqhZieUEIaS",
+    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+  }}'
 
 # Install argocd App
 kubectl apply -f ../confs/argocd-app.yaml -n argocd
-
-# Install dev App
-kubectl apply -f ../confs/deployment.yaml -n dev
